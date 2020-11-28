77f7aafdc7847951a9b1a9f47704f4ff31ee8214
kyo-kutsuzawa <k.kutsuzawa.430@gmail.com>
2020-11-28 16:59:36 +0900

    Implement update_delays()

--- src/timevarying.py
+++ src/timevarying.py
@@ -67,6 +67,106 @@ class TimeVaryingSynergy:
         return data
 
 
+def update_delays(synergies, amplitude, data):
+    """Find the delays using a nested matching procedure based on the cross-correlation.
+
+    The algorithm is based on [d'Avella et al., 2002].
+    """
+    # Setup variables
+    data = data.copy()
+    data_length = data.shape[1]
+    synergy_length = synergies.shape[1]
+    n_synergies = synergies.shape[0]
+
+    # Initialize delays
+    delays = np.zeros((data.shape[0], n_synergies), dtype=np.int)
+
+    # Find delay times for each data sequence
+    for n in range(data.shape[0]):
+        synergy_substracted = np.full((n_synergies,), False)  # Whether the delay time of the synergy has been found
+        for k in range(n_synergies):
+            # Compute correlation functions for each synergies
+            corrs = []
+            for k in range(n_synergies):
+                # Initialize the cross-correlation between the n-th data and k-th synergy.
+                # Note that the minimum possible value is zero.
+                corr = -np.ones((data_length-synergy_length+1,))
+
+                # Compute the cross-correlation if its delay has not been found yet
+                if not synergy_substracted[k]:
+                    for m in range(data.shape[2]):
+                        corr += np.correlate(data[n, :, m], synergies[k, :, m])
+                corrs.append(corr)
+
+            # Select the synergy and delay with highest cross-correlation
+            corrs = np.array(corrs)
+            (idx_synergy, delay) = np.unravel_index(np.argmax(corrs), corrs.shape)
+            delays[n, idx_synergy] = delay
+
+            # Substract the scaled and shifted synergy from the data
+            coef = corrs[idx_synergy, delay] / synergy_length
+            data[n, delay:delay+synergy_length, :] -= coef * synergies[idx_synergy, :, :]
+            synergy_substracted[idx_synergy] = True
+
+    return delays
+
+
+def _example_update_delays():
+    import matplotlib.pyplot as plt
+
+    # Setup constants
+    N =  3  # Number of data
+    M =  3  # Number of DoF
+    T = 30  # Time length of data
+    K =  2  # Number of synergies
+    S = 15  # Time length of synergies
+
+    # Create a dataset with shape (N, T, M)
+    data, synergies, (amplitude, delays) = _generate_example_data(N, M, T, K, S, plot=False)
+
+    # Estimate delays
+    delays_est = update_delays(synergies, amplitude, data)
+
+    # Print the results
+    print("Actual:\n", delays)
+    print("Expect:\n", delays_est)
+    print("Residual:\n", delays - delays_est)
+
+    # Reconstruct the data
+    data_est = np.zeros_like(data)
+    for n in range(N):
+        for k in range(K):
+            ts = delays_est[n, k]
+            data_est[n, ts:ts+S, :] += amplitude[n, k] * synergies[k, :, :]
+
+    # Plot synergy components
+    fig = plt.figure()
+    fig.suptitle("Synergy components")
+    for k in range(K):
+        for m in range(M):
+            ax = fig.add_subplot(M, K, m*K+k+1)
+            ax.plot(np.arange(synergies.shape[1]), synergies[k, :, m])
+            ax.set_xlim((0, synergies.shape[1]-1))
+            if k == 0:
+                ax.set_ylabel("DoF #{}".format(m+1))
+        ax.set_xlabel("synergy #{}".format(k+1))
+    fig.tight_layout(rect=[0, 0, 1, 0.96])
+
+    # Plot reconstruction data
+    fig = plt.figure()
+    fig.suptitle("Original/Reconstruction data")
+    axes = [fig.add_subplot(M, 1, m+1) for m in range(M)]
+    for n in range(N):
+        for m, ax in enumerate(axes):
+            ax.plot(np.arange(data.shape[1]), data[n, :, m], "--", lw=2, color=plt.get_cmap("viridis")((N-n)/(N+1)))
+            ax.plot(np.arange(data.shape[1]), data_est[n, :, m],   lw=1, color=plt.get_cmap("viridis")((N-n)/(N+1)))
+            ax.set_xlim((0, data.shape[1]-1))
+            ax.set_ylabel("DoF #{}".format(m+1))
+    fig.tight_layout(rect=[0, 0, 1, 0.96])
+
+    plt.show()
+
+
 def _example():
     """Check synergy extraction code.
     """
